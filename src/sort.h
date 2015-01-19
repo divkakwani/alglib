a4e9398b48c32e952591317ce204ebb9950a8cc3
= <divkakwani@gmail.com>
2015-01-19 23:38:47 +0530

    quick, merge sort added

--- src/sort.h
+++ src/sort.h
@@ -4,7 +4,7 @@ The functions are templatized by the iterator type. A sorting function in genera
 		<sorting_algorithm>(BEGIN_ITERATOR, END_ITERATOR, GREATER functor);
 
 Author : Divyanshu Kakwani
-Sorting Algorithms : 
+Sorting Algorithms :
 	Comparsion Sorting Algorithms
 		Bubble Sort, Insertion sort, Selection sort, Merge sort, Quicksort, Shell sort, Heap sort
 Compatibility : C++11 onwards
@@ -21,16 +21,53 @@ Compatibility : C++11 onwards
 
 
 
-template<typename _RandomAccessIterator>
-void selection_sort(_RandomAccessIterator _first, _RandomAccessIterator _last) {
+template<typename _RandomAccessIter>
+void selection_sort(_RandomAccessIter _first, _RandomAccessIter _last) {
 	for(auto it = _first; it != _last; it++)
 		std::swap(*it, *std::min_element(it, _last));
 }
 
-
-template<typename _RandomAccessIterator>
-void quick_sort(_RandomAccessIterator _first, _RandomAccessIterator _last) {
+// Quick sort helper function
+template<typename _RandomAccessIter>
+_RandomAccessIter randomized_partition(_RandomAccessIter _first, _RandomAccessIter _last) {
+	_RandomAccessIter it = _first + rand() % (_last - _first);	// O(n) cost
+	_RandomAccessIter pivot = _last - 1;
+	std::swap(*pivot, *it);
+	_RandomAccessIter sep = _first;	// marks the starting of the larger-elements partition
+	for(it = _first; it != _last; it++) {
+		if(*it < *pivot) {
+			std::swap(*it, *sep);
+			sep++;
+		}
+	}
+	std::swap(*pivot, *sep);
+	return sep;
 }
+
+template<typename _RandomAccessIter>
+void quick_sort(_RandomAccessIter _first, _RandomAccessIter _last) {
+	if(_first < _last) {
+		_RandomAccessIter pivot = randomized_partition(_first, _last);
+		quick_sort(_first, pivot);
+		quick_sort(pivot + 1, _last);
+	}
+}
+
+
+
+template<typename RandomAccessIter>
+void merge_sort(RandomAccessIter _first, RandomAccessIter _last) {
+	int sz = _last - _first;
+	if(sz > 1) {
+		std::vector<int> subarr1, subarr2;
+		std::copy(_first, _first + sz / 2, back_inserter(subarr1));
+		std::copy(_first + sz / 2, _last, back_inserter(subarr2));
+		merge_sort(subarr1.begin(), subarr1.end());
+		merge_sort(subarr2.begin(), subarr2.end());
+		std::merge(subarr1.begin(), subarr1.end(), subarr2.begin(), subarr2.end(), _first);
+	}
+}
+
 
 
 
--- test/test_sort.cpp
+++ test/test_sort.cpp
@@ -18,7 +18,7 @@ class SortTest : public Test {
 		SortTest(void (*sort_routine)(vector<int>::iterator first, vector<int>::iterator last), int tst_count);
 		void run_tests();
 		float get_runtime();
-		
+
 };
 
 SortTest::SortTest(void (*sort_routine)(vector<int>::iterator first, vector<int>::iterator last), int tst_count) : test_count(tst_count) {
@@ -34,8 +34,7 @@ void SortTest::run_tests() {
 		vector<int> subject_sorted_test = subject;
 		sort(subject_sorted_std.begin(), subject_sorted_std.end());	// Sorting through STL function
 		(*func)(subject_sorted_test.begin(), subject_sorted_test.end());	// Sorting through function to be tested;
-		
-		
+
 		equal_to<vector<int>> predicate;
 		assertPredicate(subject_sorted_std, subject_sorted_test, predicate);
 	}
@@ -44,39 +43,56 @@ void SortTest::run_tests() {
 float SortTest::get_runtime() {
 	DataGenerator<int, vector> dg;
 	vector<vector<int>> tests;
-	
+
 	// Fill the test vectors with pseudorandom integers
 	for(int n = 0; n < test_count; n++)
-		tests.insert(tests.end(), dg(1000)); 
-		
-		
+		tests.insert(tests.end(), dg(1000));
+
+
 	clock_t start_time = clock();	// start the timer
-	
+
 	for(auto& test : tests)
 		func(test.begin(), test.end());
-		
+
 	clock_t end_time = clock();		// stop the timer
-	
+
 	return float(end_time - start_time) / CLOCKS_PER_SEC;
 
 }
 
 int main() {
 
-	SortTest st(selection_sort, 100);
-	st.run_tests();
-	cout << "Time taken for selection sort : " << st.get_runtime() << endl;
-	st.report();
-	
+	// Selection sort test
+	SortTest st1(selection_sort, 100);
+	st1.run_tests();
+	cout << "Time taken for selection sort : " << st1.get_runtime() << endl;
+	st1.report();
+
 	cout << endl;
-	
-	SortTest st2(quick_sort, 10);
+
+	// Quick sort test
+	SortTest st2(quick_sort, 100);
 	st2.run_tests();
 	cout << "Time taken for quick sort : " << st2.get_runtime() << endl;
 	st2.report();
-	
+
 	cout << endl;
-	
+
+	// Standard sort test
+	SortTest st3(sort, 100);
+	st3.run_tests();
+	cout << "Time taken for std sort : " <<  st3.get_runtime() << endl;
+	st2.report();
+
+	cout << endl;
+
+	// Merge sort test
+	SortTest st4(merge_sort, 100);
+	st4.run_tests();
+	cout << "Time taken for merge sort : " << st4.get_runtime() << endl;
+	st4.report();
+
+	cout << endl;
+
 	return 0;
 }
-
