a6dccf16f0eedca8a3b0c257df1755c3ddcc650a
= <divkakwani@gmail.com>
2015-01-30 23:52:41 +0530

    naive substring search added

--- src/algorithms/str_matching.h
+++ src/algorithms/str_matching.h
@@ -1,6 +1,7 @@
 #ifndef _STR_MATCHING_
 #define _STR_MATCHING_
 
+#include <string>
 
 /*
 
@@ -13,7 +14,21 @@
 
 */
 
-
+int naive_str_matching(std::string text, std::string pattern) {
+	/**
+		The naive algorithm checks for the equivalence of pattern P
+		with the substring of the text T[s:s+m-1] starting with the
+		shift s = 0 and going all the way to s = n-m.
+		
+	**/
+	
+	int s = 0;
+	int n = text.length(), m = pattern.length();
+	for(s = 0; s <= n - m; s++) {
+		if(text.substr(s, s + m) == pattern)
+			return s;
+	return string::npos;
+}
 
 
 #endif
