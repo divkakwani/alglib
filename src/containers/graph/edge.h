972a3aa729e4ca5f4b49c07ed64cc12d6c4e4d74
= <divkakwani@gmail.com>
2015-04-20 18:32:55 +0530

    redesign graph class hierarchy

--- src/containers/graph/edge.h
+++ src/containers/graph/edge.h
@@ -4,15 +4,19 @@
 
 template<typename vertex_t>
 class edge {
+
 	public:
 		virtual vertex_t& from() const = 0;
 		virtual vertex_t& to() const = 0;
+
 };
 
 template<typename vertex_t, typename weight_t>
 class weighted_edge : public edge<vertex_t> {
+
 	public:
 		virtual weight_t weight() const;
+
 };
 
 #endif
