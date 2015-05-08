bebc65be3c6332581241fe11eca49041389d6d06
= <divkakwani@gmail.com>
2015-05-08 12:55:36 +0530

    Changes in heap interface; binary_heap remake

--- src/containers/heap/binary_heap.h
+++ src/containers/heap/binary_heap.h
@@ -1,102 +1,123 @@
+/*
+ * This file is part of the alglib project.
+ *
+ * (c) Divyanshu Kakwani <divkakwani@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE file
+ * that was distributed with this source code.
+ */
+
+#ifndef _BINARY_HEAP_H
+#define _BINARY_HEAP_H
+
+
 #include <iostream>
-#include "heap.h"
 #include <vector>
 #include <algorithm>
+#include <functional>
+#include "heap.h"
 
-// binary_heap interface
-// 	1. insert
-//	2. extract_min/ extract_max
-//	3. create_heap
-//	4. 
 
-template<typename elt_type>
-class binary_heap : min_heap<elt_type> {
+template<typename elt_type, typename Compare = less<elt_type>>
+class binary_heap : public min_heap<elt_type, Compare> {
+ public:
+    binary_heap();
 
-		std::vector<elt_type> __stash;
-		int __sz;
-		
-		/** Helper functions */
-		inline int __parent(int node);
-		inline int __left_child(int node);
-		inline int __right_child(int node);
-		void __bubble_up(int node);
-		void __bubble_down(int node);
-		
-	public:
-		binary_heap(int sz = 0);
-		// add a ctor for initialization through an array.
-		void insert(elt_type elt);
-		elt_type extract_min();		
+    /* aggregate initialization */
+    template<typename InputIter>
+    binary_heap(InputIter first, InputIter last);
+
+    inline void reserve(int sz) { __stash.reserve(sz); }
+    int size() const override { return __sz; }
+    bool empty() const override { return __sz == 0; }
+
+ private:
+    std::vector<elt_type> __stash;
+    int __sz;
+    Compare comp;
+
+    /* Helper functions */
+    inline int __parent(int node) { return (node - 1) / 2; }
+    inline int __left_child(int node) { return node * 2 + 1; }
+    inline int __right_child(int node) { return node * 2 + 2;}
+    void __sift_up(int node);
+    void __sift_down(int node);
 };
 
-template<typename elt_type>
-binary_heap<elt_type>::binary_heap(int sz) {
-	__sz = sz;
-	__stash.reserve(sz);
-	
+template<typename elt_type, typename Compare>
+binary_heap<elt_type, Compare>::binary_heap() {
+    __sz = 0;
 }
 
-template<typename elt_type>
-void binary_heap<elt_type>::insert(elt_type elt) {
+template<typename elt_type, typename Compare>
+template<typename InputIter>
+binary_heap<elt_type, Compare>::binary_heap<InputIter>(InputIter first,
+                                                       InputIter last) {
+    std::copy(first, last, back_inserter(__stash));
+    __sz = __stash.size();
 
-	__stash.push_back(elt);
-	__bubble_up(__sz);
-	++__sz;
+    // Heapify the __stash
+    for (int i = __sz / 2; i >= 0; i++)
+         __sift_down(i);
 }
 
-template<typename elt_type>
-elt_type binary_heap<elt_type>::extract_min() {
-	
-	elt_type elt = __stash[0];
-	std::swap(__stash[0], __stash[__sz - 1]);	// FIXME : handle the dtor
-	--__sz;
-	__bubble_down(0);
-	return elt;
-}
-
-template<typename elt_type>
-void binary_heap<elt_type>::__bubble_up(int node) {
-	int parent = __parent(node);
-	while(parent >= 0 and __stash[parent] > __stash[node]) {
-		std::swap(__stash[parent], __stash[node]);
-		node = parent;
-		parent = __parent(node);
-	}
-
-}
-
-template<typename elt_type>
-void binary_heap<elt_type>::__bubble_down(int node) {
-	int min_idx = node;
-	int left_idx = __left_child(node);
-	int right_idx = __right_child(node);
-	
-	if(left_idx < __sz and __stash[left_idx] < __stash[min_idx])
-		min_idx = left_idx;
-	if(right_idx < __sz and __stash[right_idx] < __stash[min_idx])
-		min_idx = right_idx;
-	
-	if(min_idx != node) {
-		std::swap(__stash[node], __stash[min_idx]);
-		__bubble_down(min_idx);
-	}
-}
-
-template<typename elt_type>
-inline int binary_heap<elt_type>::__parent(int node) {
-	return node / 2;
-}
-
-template<typename elt_type>
-inline int binary_heap<elt_type>::__left_child(int node) {
-	return 2 * node + 1;
-}
-
-template<typename elt_type>
-inline int binary_heap<elt_type>::__right_child(int node) {
-	return 2 * node + 2;
+template<typename elt_type, typename Compare>
+void binary_heap<elt_type, Compare>::insert(const elt_type& elt) override {
+    __stash.push_back(elt);
+    __sift_up(__sz);
+    ++__sz;
 }
 
 
+template<typename elt_type, typename Compare>
+const elt_type& binary_heap<elt_type, Compare>::get_min() override {
+    return __stash[0];
+}
 
+template<typename elt_type, typename Compare>
+void binary_heap<elt_type, Compare>::delete_min() override {
+    std::swap(__stash[0], __stash[__sz - 1]);
+    --__sz;
+    __sift_down(0);
+    return elt;
+}
 
+template<typename elt_type, Compare>
+void binary_heap<elt_type, Compare>::replace(const elt_type& elt) override {
+    if (__sz == 0)
+        throw "no element";
+    __stash[0] = elt;
+    __sift_down(0);
+}
+
+template<typename elt_type, Compare>
+void binary_heap<elt_type, Compare>::update_key(const elt_type& elt) override {
+    // how?
+}
+
+template<typename elt_type, typename Compare>
+void binary_heap<elt_type, Compare>::__sift_up(int node) {
+    if (node != 0 && comp(__stash[node], __stash[__parent(node)])) {
+        std::swap(__stash[__parent(node)], __stash[node]);
+        __sift_up(__parent(node));
+    }
+}
+
+template<typename elt_type, typename Compare>
+void binary_heap<elt_type, Compare>::__sift_down(int node) {
+    int min_node = node;
+    int left_child = __left_child(node);
+    int right_child = __right_child(node);
+
+    if (left_child < __sz && comp(__stash[left_child], __stash[min_node]))
+        min_node = left_child;
+    if (right_child < __sz && comp(__stash[right_child], __stash[min_node]))
+        min_node = right_child;
+
+    if (min_node != node) {
+        std::swap(__stash[node], __stash[min_node]);
+        __sift_down(min_node);
+    }
+}
+
+#endif  // _BINARY_HEAP_H
--- src/containers/heap/heap.h
+++ src/containers/heap/heap.h
@@ -1,67 +1,51 @@
+/*
+ * This file is part of the alglib project.
+ *
+ * (c) Divyanshu Kakwani <divkakwani@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE file
+ * that was distributed with this source code.
+ */
 
 #ifndef _HEAP_H
 #define _HEAP_H
 
-// abstract class
-// Any heap must conform to this interface
+#include <boost/concept_check.hpp>
+#include <functional>
 
-/* Why does the min-heap not use operator< for ordering the elements
- * and instead use another variable `key` to order them?
- * 	
- * 	The reason of having another variable `key` for ordering the
- * 	elements is to decouple the scheme of ordering from the 
- * 	object itself.
- * 	If we use operator< of elt's class, it would be far too 
- * 	restrictive. It would allow only one type of ordering - the
- * 	one defined by the class. In many situations, that may not be
- * 	the scheme we require. Worse still, it precludes any non-comparable
- * 	item from being used to make a min-heap.
- *	To make this more concrete, consider a use-case in which a shopkeeper
- *	services the requests of his customers. The customers are of Person type
- *	that doesn't have any total-ordering relation. The shopkeeper, however, 
- *	can have different schemes of prioritizing his customers, some of them
- *	are -
- *		Prioritize the shop's investor.
- *		Prioritize senior citizens.
- *		Prioritize acquaintances
- *		... and so on.
- *	The essence of this methodology is that the criterion of ordering should 
- *	be specified by the min_heap user, not by the class.
- * 
+/*
+ * Abstract class for a heap
+ * Any heap must conform to this interface
+ *
+ * The class is configurable with different schemes of ordering
+ * by supplying corresponding `Compare` functor. It defaults to
+ * operator< in case no comparison functor is specified.
  */
 
-
-template<typename elt_t, typename key_t>
+template<typename elt_t, typename Compare = less<elt_type>>
 class min_heap {
+    BOOST_CONCEPT_ASSERT((BinaryPredicate<Compare, elt_t, elt_t>));
 
-	public:
+ public:
+    virtual void insert(const elt_t& elt) = 0;
 
-		/* typedefs */
-		typedef elt_t elt_type;
-		typedef key_t key_type;
+    /* get the root */
+    virtual const elt_t& get_min() const = 0;
 
-		virtual void insert(const elt_t& elt, const key_t& key) = 0;
+    /* remove the root */
+    virtual void delete_min() = 0;
+    virtual void replace(const elt_type& elt) = 0;
 
-		/* get the root */
-		virtual const elt_t& get_min_elt() const = 0;
-		virtual const key_t& get_min_key() const = 0;
+    /* Inspection */
+    virtual int size() const = 0;
+    virtual bool empty() const = 0;
 
-		/* remove the root */
-		virtual void delete_min() = 0;
-		virtual void replace(const elt_type& elt, const key_t& key) = 0;
+    /* Internal modification */
+    virtual void update_key(const elt_type& elt) = 0;
 
-		/* Inspection */
-		virtual int size() const = 0;
-		virtual bool empty() const = 0;
-
-		/* Internal modification */	
-		virtual void change_key(const elt_type& elt, const key_t& new_key) = 0;
-
-		/* return a new heap that is the merger of the heap h and this. */
-		min_heap<elt_t, key_t>& merge_heaps(const min_heap<elt_t, key_t>& h);
-
+    /* return a new heap that is the merger of the heap h and *this. */
+    min_heap<elt_t, Compare> merge_heaps(const min_heap<elt_t, Compare>& h);
 };
 
 
-
-#endif
+#endif // _HEAP_H
