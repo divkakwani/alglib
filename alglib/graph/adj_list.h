4081e0c2981d41809c22973d2a19ee008a0c7c8a
= <divkakwani@gmail.com>
2015-05-25 23:21:26 +0530

    Implement const_eiterator

--- alglib/graph/adj_list.h
+++ alglib/graph/adj_list.h
@@ -7,18 +7,26 @@
  * that was distributed with this source code.
  */
 
+#include <iostream>
 #include <map>
 #include <boost/iterator/transform_iterator.hpp>
-#include <alglib/containers/graph/graph_model.h>
+#include <alglib/graph/graph_model.h>
 
 #pragma once
 
-
+/**
+ * \breif A functor that returns the first element of a pair
+ *
+ */
 template<typename T>
 struct get_first {
     typename T::first_type operator() (const T& p) const { return p.first; }
 };
 
+/**
+ * \breif A functor that returns the second element of a pair
+ *
+ */
 template<typename T>
 struct get_second {
     typename T::second_type operator() (const T& p) const { return p.second; }
@@ -59,10 +67,10 @@ class adj_list : public graph_model<vertex_t, edge_t> {
                                       typename std::map<vertex_t, alist_t>::iterator>
             const_viterator;
 
-    typedef boost::transform_iterator<get_first<std::pair<vertex_t, edge_t>,
+    typedef boost::transform_iterator<get_first<std::pair<vertex_t, edge_t>>,
                                       typename std::map<vertex_t, edge_t>::iterator>
             const_aviterator;
-    typedef boost::transform_iterator<get_second<std::pair<vertex_t, edge_t>,
+    typedef boost::transform_iterator<get_second<std::pair<vertex_t, edge_t>>,
                                       typename std::map<vertex_t, edge_t>::iterator>
             const_aeiterator;
     
@@ -73,10 +81,10 @@ class adj_list : public graph_model<vertex_t, edge_t> {
     const_viterator vend();
     const_eiterator ebegin();
     const_eiterator eend();
-    //const_aviterator avbegin(const vertex_t& v);
-    //const_aviterator avend(const vertex_t& v);
-    //const_aeiterator aebegin(const vertex_t& v);
-    //const_aeiterator aeend(const vertex_t& v);
+    const_aviterator avbegin(const vertex_t& v);
+    const_aviterator avend(const vertex_t& v);
+    const_aeiterator aebegin(const vertex_t& v);
+    const_aeiterator aeend(const vertex_t& v);
 
 
  private:
@@ -188,58 +196,103 @@ inline typename adj_list<vertex_t, edge_t>::const_viterator adj_list<vertex_t, e
 
 template<typename vertex_t, typename edge_t>
 inline typename adj_list<vertex_t, edge_t>::const_aviterator adj_list<vertex_t, edge_t>::avbegin(const vertex_t& u) {
-    get_first<pair<vertex_t, edge_t>> trans;
+    get_first<std::pair<vertex_t, edge_t>> trans;
     return boost::make_transform_iterator(alists[u].begin(), trans);
 }
 
 
 
 template<typename vertex_t, typename edge_t>
-inline typename adj_list<vertex_t, edge_t>::const_aviterator adj_list<vertex_t, edge_t>::avend() { 
-    get_first<pair<vertex_t, edge_t>> trans;
+inline typename adj_list<vertex_t, edge_t>::const_aviterator adj_list<vertex_t, edge_t>::avend(const vertex_t& u) { 
+    get_first<std::pair<vertex_t, edge_t>> trans;
     return boost::make_transform_iterator(alists[u].end(), trans);
 }
 
 
 template<typename vertex_t, typename edge_t>
 inline typename adj_list<vertex_t, edge_t>::const_aeiterator adj_list<vertex_t, edge_t>::aebegin(const vertex_t& u) {
-    get_second<pair<vertex_t, edge_t>> trans;
+    get_second<std::pair<vertex_t, edge_t>> trans;
     return boost::make_transform_iterator(alists[u].begin(), trans);
 }
 
 
 
 template<typename vertex_t, typename edge_t>
-inline typename adj_list<vertex_t, edge_t>::const_aeiterator adj_list<vertex_t, edge_t>::aeend() { 
-    get_second<pair<vertex_t, edge_t>> trans;
+inline typename adj_list<vertex_t, edge_t>::const_aeiterator adj_list<vertex_t, edge_t>::aeend(const vertex_t& u) { 
+    get_second<std::pair<vertex_t, edge_t>> trans;
     return boost::make_transform_iterator(alists[u].end(), trans);
 }
 
 
 template<typename vertex_t, typename edge_t>
-inline typename adj_list<vertex_t, edge_t>::const_eiterator adj_list<vertex_t, edge_t>::ebegin(const vertex_t& u) {
-    get_first<pair<vertex_t, edge_t>> trans;
-    return boost::make_transform_iterator(alists[u].begin(), trans);
+inline typename adj_list<vertex_t, edge_t>::const_eiterator adj_list<vertex_t, edge_t>::ebegin() {
+    return const_eiterator(*this);
 }
 
 
 
 template<typename vertex_t, typename edge_t>
 inline typename adj_list<vertex_t, edge_t>::const_eiterator adj_list<vertex_t, edge_t>::eend() { 
-    get_first<pair<vertex_t, edge_t>> trans;
-    return boost::make_transform_iterator(alists[u].end(), trans);
+    const_eiterator it(*this);
+    it.vit = vend();
+    return it;
 }
 
 
 template<typename vertex_t, typename edge_t>
 class adj_list<vertex_t, edge_t>::const_eiterator {
 
-        // Write an iterator class
+ private:
+    const_aeiterator aeit;
+    const_viterator  vit;
+    adj_list<vertex_t, edge_t>& object;
+
+    friend class adj_list<vertex_t, edge_t>;
+
+    friend std::ostream& operator<<(std::ostream& out, const_eiterator eit) {
+        out << *(eit.aeit);
+        return out;
+    }
+
+    const_eiterator() { }
+
+ public:
+    
+
+    const_eiterator(adj_list<vertex_t, edge_t>& obj) : object(obj) {
+        if(obj.alists.size() != 0) {
+            vit = obj.vbegin();
+            aeit = obj.aebegin(*vit);
+        }
+    }
+
+    const_eiterator(const const_eiterator& eit) : object(eit.object){
+        vit = eit.vit;
+        aeit = eit.aeit;
+
+    }
+
+
+    void operator++() {
+        aeit++;
+        if(aeit == object.aeend(*vit)) {
+            vit++;
+            aeit = object.aebegin(*vit);
+        }
+        //return *aeit;  // FIXME: set to previos aeit;
+    }
+
+    const edge_t& operator*() {
+        return *aeit;
+    }
+
+    bool operator==(const const_eiterator& other) const {
+        return (vit == other.vit && aeit == other.aeit) || (vit == other.vit && vit == object.vend());
+    } 
+
+    bool operator!=(const const_eiterator& other) const {
+        return !(vit == other.vit && aeit == other.aeit) && !(vit == object.vend() && vit == other.vit);
+    }
 
 };
-
-
-
-
-
 
--- test/graph/adj_list.cpp
+++ test/graph/adj_list.cpp
@@ -1,5 +1,5 @@
-#include <alglib/containers/graph/adj_list.h>
-#include <alglib/containers/graph/edge.h>
+#include <alglib/graph/adj_list.h>
+#include <alglib/graph/edge.h>
 #include <iostream>
 
 using namespace std;
@@ -8,15 +8,22 @@ using namespace std;
 int main() {
 
     adj_list<int, edge_t<int>> al;
-
+    
     edge_t<int> e;
-    e.from = 3, e.to = 3, e.attribute = 100;
+    e.from = 3, e.to = 4, e.attribute = 100;
 
     al.add_vertex(4);
     al.add_vertex(3);
+    al.add_vertex(10);
     al.add_edge(e);
 
     for(auto it = al.vbegin(); it != al.vend(); it++)
         cout << *it << endl;
+
+    cout << "Vertices adjacent to 4: \n";
+
+    cout << *al.ebegin() << endl;
+    //for(auto it = al.ebegin(); it != al.eend(); ++it)
+            cout << "this"; 
 
 }
