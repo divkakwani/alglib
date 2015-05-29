0b9a7b7e74750b6775db8b78df5fe4fed65e26f1
= <divkakwani@gmail.com>
2015-05-29 22:44:07 +0530

    Remove the derived edge_t type

--- alglib/graph/edge.h
+++ alglib/graph/edge.h
@@ -11,38 +11,51 @@
 
 #include <type_traits>
 
-template<typename vertex_t, typename attr_t = int, typename Enable = void>
-struct edge_t : public attr_t {
-
-    vertex_t from;
-    vertex_t to;
-};
-
-
 template<typename vertex_t, typename attr_t>
-struct edge_t<vertex_t, attr_t,
-              typename std::enable_if<std::is_fundamental<attr_t>::value>::type> {
+struct edge_t {
               
     vertex_t from;
     vertex_t to;
     attr_t attribute;
+
+    edge_t(const vertex_t& u, const vertex_t& v, const vertex_t& attr) {
+        from = u;
+        to = v;
+        attribute = attr;
+    }
+    
+    bool operator==(const edge_t<vertex_t, edge_t>& other) const
+    { return attribute == other.attribute; }
+
+    bool operator!=(const edge_t<vertex_t, edge_t>& other) const 
+    { return attribute != other.attribute; }
+
+    bool operator>(const edge_t<vertex_t, edge_t>& other) const 
+    { return attribute > other.attribute; }
+
+    bool operator<(const edge_t<vertex_t, edge_t>& other) const 
+    { return attribute < other.attribute; }
+
+    bool operator>=(const edge_t<vertex_t, edge_t>& other) const 
+    { return attribute >= other.attribute; }
+
+    bool operator<=(const edge_t<vertex_t, edge_t>& other) const 
+    { return attribute <= other.attribute; }
 };
 
 template<typename vertex_t, typename attr_t>
 std::ostream& operator<<(std::ostream& out, const edge_t<vertex_t, attr_t>& e) {
 
-    std::cout << "(" << e.from << ", " << e.to << ") ";
+    out << "(from: " << e.from << ", to: " << e.to
+        << ", attr: " << e.attribute << ")";
     return out;
 }
 
-/*
 
 template<typename vertex_t, typename attr_t>
-edge_t<vertex_t, attr_t> make_edge (vertex_t from, vertex_t to,
-                                    attr_t attribute = -1) {
-    return edge_t<vertex_t, attr_t> {from, to, attribute};
+edge_t<vertex_t, attr_t> make_edge(vertex_t from, vertex_t to,
+                                   attr_t attribute) {
+    return edge_t<vertex_t, attr_t>(from, to, attribute);
 }
-*/
-
 
 
