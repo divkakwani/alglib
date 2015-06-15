3995042bb8b21af7f1b0cf1e80418da127efe288
= <divkakwani@gmail.com>
2015-06-15 23:20:50 +0530

    fix build

--- test/sort/test_sort.cpp
+++ test/sort/test_sort.cpp
@@ -9,76 +9,84 @@
 #include <functional>
 
 using namespace std;
-
+using namespace alglib::sort;
 
 template<typename Seq>
 class SortTest : public Test {
-	void (*func)(vector<int>::iterator first, vector<int>::iterator last);
-	DataGenerator<Seq> gen;
-	public:
-		typedef typename Seq::iterator it_type;
-		SortTest(void (*sort_routine)(it_type first, it_type last));
-		void run_tests();
-		void random_tests(int tst_cnt, int inp_sz);
+    void (*func)(vector<int>::iterator first, vector<int>::iterator last);
+    DataGenerator<Seq> gen;
+    public:
+        typedef typename Seq::iterator it_type;
+        SortTest(void (*sort_routine)(it_type first, it_type last));
+        void run_tests();
+        void random_tests(int tst_cnt, int inp_sz);
 
 };
 
 template<typename Seq>
 SortTest<Seq>::SortTest(void (*sort_routine)(it_type first, it_type last)) {
-	func = sort_routine;
+    func = sort_routine;
 }
 
 template<typename Seq>
 void SortTest<Seq>::run_tests() {
 
-	random_tests(100, 100);
+    random_tests(100, 100);
 
 }
 
 template<typename Seq>
 void SortTest<Seq>::random_tests(int tst_cnt, int inp_sz) {
 
-	for(int _ = 0; _ < tst_cnt; _++) {
-	Seq subject = gen.random_data(inp_sz);
-	Seq subject_sorted_std = subject;
-	Seq subject_sorted_test = subject;
-	sort(subject_sorted_std.begin(), subject_sorted_std.end());	// Sorting through STL function
-	(*func)(subject_sorted_test.begin(), subject_sorted_test.end());	// Sorting through function to be tested;
+    for(int _ = 0; _ < tst_cnt; _++) {
+    Seq subject = gen.random_data(inp_sz);
+    Seq subject_sorted_std = subject;
+    Seq subject_sorted_test = subject;
+    sort(subject_sorted_std.begin(), subject_sorted_std.end()); // Sorting through STL function
+    (*func)(subject_sorted_test.begin(), subject_sorted_test.end());    // Sorting through function to be tested;
 
-	assertPredicate(subject_sorted_std, subject_sorted_test, equal_to<vector<int>>());
-	}
+    assertPredicate(subject_sorted_std, subject_sorted_test, equal_to<vector<int>>());
+    }
 }
 
 int main() {
 
 
-	// Selection sort test
-	SortTest<vector<int>> st1(selection_sort);
-	st1.run_tests();
-	st1.report();
+    // Selection sort test
+    SortTest<vector<int>> st1(selection_sort);
+    st1.run_tests();
+    st1.report();
 
-	cout << endl;
+    cout << endl;
 
-	// Quick sort test
-	SortTest<vector<int>> st2(quick_sort);
-	st2.run_tests();
-	st2.report();
+    // Quick sort test
+    SortTest<vector<int>> st2(quick_sort);
+    st2.run_tests();
+    st2.report();
 
-	cout << endl;
+    cout << endl;
 
-	// Standard sort test
-	SortTest<vector<int>> st3(sort);
-	st3.run_tests();
-	st2.report();
+    // Standard sort test
+    SortTest<vector<int>> st3(sort);
+    st3.run_tests();
+    st2.report();
 
-	cout << endl;
+    cout << endl;
 
-	// Merge sort test
-	SortTest<vector<int>> st4(merge_sort);
-	st4.run_tests();
-	st4.report();
+    // Merge sort test
+    SortTest<vector<int>> st4(merge_sort);
+    st4.run_tests();
+    st4.report();
 
-	cout << endl;
+    cout << endl;
 
-	return st1.get_status() | st2.get_status() | st3.get_status() | st4.get_status();
+    /* SortTest<vector<int>> st5(heap_sort); */
+    /* st5.run_tests(); */
+    /* st5.report(); */
+
+    /* cout << endl; */
+
+    return st1.get_status() | st2.get_status() | st3.get_status() |
+           st4.get_status();
 }
+
